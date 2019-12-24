5556f3681df3bd702fa77291da82a5351dc4c68b
unknown <jamespaulmoriarty@gmail.com>
2019-12-24 16:22:49 +1100

    refinement: better systemParametersInfo logs.

--- main.go
+++ main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"syscall"
 	"unsafe"
+)
+
+const (
+	// fWinIni
+	SPIF_UPDATEINIFILE    = 0x0
+	SPIF_SENDCHANGE       = 0x1
+	SPIF_SENDWININICHANGE = 0x2
+
+	// uiAction
+	SPI_SETDESKWALLPAPER = 0x0014 // SPI_SETDESKWALLPAPER 0x0014 - Note  When the SPI_SETDESKWALLPAPER flag is used, SystemParametersInfo returns TRUE unless there is an error (like when the specified file doesn't exist).
 )
 
 var (
 	url                  = "https://source.unsplash.com/collection/220388/1920x1080"
 	user32               = syscall.NewLazyDLL("user32.dll")
 	systemParametersInfo = user32.NewProc("SystemParametersInfoW") // https://docs.microsoft.com/en-gb/windows/win32/api/winuser/nf-winuser-systemparametersinfow
-	uiAction             = 0x0014                                  // SPI_SETDESKWALLPAPER 0x0014 - Note  When the SPI_SETDESKWALLPAPER flag is used, SystemParametersInfo returns TRUE unless there is an error (like when the specified file doesn't exist).
 )
+
+func toHex(ptr uintptr) string {
+	s := fmt.Sprintf("%d", ptr)
+	n, _ := strconv.Atoi(s)
+	h := fmt.Sprintf("0x%x", n)
+	return h
+}
 
 func main() {
 	log.SetFormatter(&log.TextFormatter{ForceColors: true})
@@ -61,8 +79,10 @@ func main() {
 	}
 
 	pvParam := uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(path)))
+	uiAction := uintptr(SPI_SETDESKWALLPAPER)
+	fWinIni := uintptr(SPIF_SENDWININICHANGE)
 
-	log.WithFields(log.Fields{"uiAction": uiAction, "uiParam": 0, "pvParam": pvParam}).Info("SystemParametersInfoW")
+	log.WithFields(log.Fields{"uiAction": toHex(uiAction), "uiParam": toHex(0), "pvParam": toHex(pvParam), "fWinIni": toHex(fWinIni)}).Info("SystemParametersInfoW")
 
-	systemParametersInfo.Call(uintptr(uiAction), 0, pvParam, 2)
+	systemParametersInfo.Call(uintptr(uiAction), 0, uintptr(pvParam), uintptr(fWinIni))
 }
