3d831caa8b3ee1371f3721a3dc00801a6785d3d7
unknown <jamespaulmoriarty@gmail.com>
2019-12-28 22:23:45 +1100

    feature: reg edit works now with advapi32.dll.

--- main_windows_amd64.go
+++ main_windows_amd64.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 	"syscall"
 	"time"
+	"unicode/utf16"
 	"unsafe"
 )
 
@@ -31,6 +32,36 @@ func toHex(ptr uintptr) string {
 	n, _ := strconv.Atoi(s)
 	h := fmt.Sprintf("0x%x", n)
 	return h
+}
+
+func errstr(errno int32) string {
+	// ask windows for the remaining errors
+	var flags uint32 = syscall.FORMAT_MESSAGE_FROM_SYSTEM | syscall.FORMAT_MESSAGE_ARGUMENT_ARRAY | syscall.FORMAT_MESSAGE_IGNORE_INSERTS
+	b := make([]uint16, 300)
+	n, err := syscall.FormatMessage(flags, 0, uint32(errno), 0, b, nil)
+	if err != nil {
+		return fmt.Sprintf("error %d (FormatMessage failed with: %v)", errno, err)
+	}
+	// trim terminating \r and \n
+	for ; n > 0 && (b[n-1] == '\n' || b[n-1] == '\r'); n-- {
+	}
+	return string(utf16.Decode(b[:n]))
+}
+
+func setRegString(dir string, key string, value string) {
+	var handle win.HKEY
+
+	ret := win.RegOpenKeyEx(win.HKEY_CURRENT_USER, syscall.StringToUTF16Ptr(dir), 0, syscall.KEY_WRITE, &handle)
+
+	if ret != 0 {
+		panic(fmt.Sprintf("RegOpenKeyEx: %s", errstr(ret)))
+	}
+
+	ret = win.RegSetValueEx(handle, syscall.StringToUTF16Ptr(key), 0, win.REG_SZ, (*byte)(unsafe.Pointer(syscall.StringToUTF16Ptr(value))), 32)
+
+	if ret != 0 {
+		panic(fmt.Sprintf("RegSetValueEx: %s", errstr(ret)))
+	}
 }
 
 func getRandomDesktopWallpaperPath() (string, error) {
@@ -88,9 +119,17 @@ func setDesktopWallpaper(path string) error {
 	uiAction := uint32(SPI_SETDESKWALLPAPER)
 	fWinIni := uint32(SPIF_SENDWININICHANGE)
 
-	log.WithFields(log.Fields{"uiAction": uiAction, "uiParam": 0, "pvParam": toHex(uintptr(pvParam)), "fWinIni": fWinIni}).Info("SystemParametersInfoW")
+	log.WithFields(log.Fields{"uiAction": uiAction, "uiParam": 0, "pvParam": toHex(uintptr(pvParam)), "fWinIni": fWinIni, "dll": "user32"}).Info("SystemParametersInfoW")
 
 	ret := win.SystemParametersInfo(uiAction, uiParam, pvParam, fWinIni)
+
+	dir := "Control Panel\\Desktop"
+	key := "WallpaperStyle"
+	value := "10" // Fill
+
+	log.WithFields(log.Fields{"dir": dir, "key": 0, "value": value, "dll": "advapi32"}).Info("RegSetValueEx")
+
+	setRegString(dir, key, value)
 
 	if ret != true {
 		return errors.New("Failed setting desktopwallpaper")
